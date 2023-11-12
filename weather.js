f07dfe2ecf341d8d6021d354b0bbea27a3dca5f3
Jaeho Jang <jjho10@naver.com>
2023-11-12 14:08:01 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -5,7 +5,6 @@ const API_KEY = "eedcd9aa330fecfc049862ae98148079";
 function onGeoPass(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log(lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(response => response.json()
